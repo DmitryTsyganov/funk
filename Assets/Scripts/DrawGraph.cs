9594c70926edafaf799dcc46e44d9264de543b5b
DmitryTsyganov <dmitrytsgn@gmail.com>
2017-02-20 01:23:38 +0330

    fix missing colliders in Unity 5.6

--- Assets/Scripts/DrawGraph.cs
+++ Assets/Scripts/DrawGraph.cs
@@ -163,7 +163,7 @@ public class DrawGraph : MonoBehaviour
             //print("first x " + firstX);
             //print("last x " + lastX);
 
-            int stepsTotal = 260;
+            int stepsTotal = 90;
 
             double stepLength = (-firstX + lastX) / stepsTotal;
 
