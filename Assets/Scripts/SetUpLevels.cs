1f73a5eb38d3d44af6bb68a7826efe7859ed041a
DmitryTsyganov <dmitrytsgn@gmail.com>
2017-04-05 01:14:19 +0330

    scroll levels menu, fixes #38

--- Assets/Scripts/SetUpLevels.cs
+++ Assets/Scripts/SetUpLevels.cs
@@ -8,23 +8,46 @@ using UnityEngine.UI;
 public class SetUpLevels : MonoBehaviour {
 
 	public levelElement LevelElementPrefab;
-   
+    public ScrollRect LevelScrollRect;
     
 	void Start () {
 		LoadLevels ();
     }
+
 	void LoadLevels(){
-		int count =  Convert.ToInt32(Resources.Load<TextAsset> (
+		float count =  Convert.ToInt32(Resources.Load<TextAsset>(
                     "Levels" + "/" + ScenesParameters.Section + "/" + "config").text);
+
+	    bool isFirstNotCompletedLevel = true;
+
+	    float position = 0;
+
 		for (int i = 1; i <= count; i++) {
-			CreateLevelElement (i);
+			CreateLevelElement(i);
+		    if (isFirstNotCompletedLevel && !Saver.isLevelCompletedWithStars(i) && !Saver.isLevelComplete(i))
+		    {
+		        isFirstNotCompletedLevel = false;
+		        position = 1f - Mathf.Ceil(i / 2f) / Mathf.Ceil(count / 2f);
+		        if (position < 0.15) position = 0;
+		        if (position > 0.15 && position < 0.5) position *= 0.85f;
+		        if (position > 0.5f) position *= 1.2f;
+		        print($"New position is {position}, current position is" +
+		              $" {LevelScrollRect.verticalNormalizedPosition}");
+		    }
 		}
+	    LevelScrollRect.verticalNormalizedPosition = position;
 	}
-	void CreateLevelElement(int index){
+
+    private void Update()
+    {
+        print($" {LevelScrollRect.verticalNormalizedPosition}");
+    }
+
+    void CreateLevelElement(int index){
 		Transform element = Instantiate (LevelElementPrefab).transform;
 		element.SetParent (transform);
 		element.localScale = Vector3.one;
-		element.gameObject.GetComponent<levelElement> ().SetLevelIndex (index);
+		element.gameObject.GetComponent<levelElement>().SetLevelIndex(index);
 	}
 
 
