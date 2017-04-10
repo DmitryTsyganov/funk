bcaa7b10d9b1b06297119e1409355fdca320e17f
DmitryTsyganov <dmitrytsgn@gmail.com>
2017-04-11 01:25:39 +0330

    add missing scripts

---
+++ Assets/Scripts/InfoTextHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoTextHandler : MonoBehaviour
+{
+
+    public GameObject BackText;
+
+	// Use this for initialization
+	void Start () {
+		setLanguage();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void Click()
+    {
+        Destroy(gameObject);
+    }
+
+    void setLanguage()
+    {
+        LanguageManager.setLanguageIfNotAlready();
+        LanguageManager.setText(BackText, LanguageManager.getLanguage().back);
+    }
+}
