48ca86ba3abe854e16f891b3cafd1829ab54c695
DmitryTsyganov <dmitrytsgn@gmail.com>
2017-04-25 03:35:21 +0330

    coefficient before an letter

--- Assets/Scripts/DrawGraph.cs
+++ Assets/Scripts/DrawGraph.cs
@@ -222,7 +222,7 @@ public class DrawGraph : MonoBehaviour
                 //49 steps faster WOW WOW
                 rawExp = Regex.Replace(rawExp, @"([\/\*])([\-\+]\d*\.?\d*(([a-z])?(?![a-z])|([a-z]){3}\(.+?\)))", "$1($2)");
 
-                rawExp = Regex.Replace(rawExp, @"(\d(?=[ascxl\(])|[x\)](?=\d))", "$0*");
+                rawExp = Regex.Replace(rawExp, @"(\d(?=[a-z\(])|[x\)](?=\d))", "$0*");
                 rawExp = Regex.Replace(rawExp, @"(?<![0-9\.])0[0-9]+", "0");
 
                 Expression exp = parser.EvaluateExpression(rawExp);
