f2ef728da171a218947471cafc7e14e73ef392d7
DmitryTsyganov <dmitrytsgn@gmail.com>
2017-05-17 00:49:43 +0330

    update reset button

---
+++ Assets/Scripts/ResetButtonHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetButtonHandler : MonoBehaviour
+{
+
+	public GameObject ResetText;
+
+	// Use this for initialization
+	void Start () {
+		setLanguage();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void setLanguage()
+	{
+		LanguageManager.setLanguageIfNotAlready();
+		LanguageManager.setText(ResetText, LanguageManager.getLanguage().reset);
+	}
+}
