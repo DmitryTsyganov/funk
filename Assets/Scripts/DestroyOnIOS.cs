3fe7d1d91fd1bd4fb1d4cad3e13b58b2d71be81b
DmitryTsyganov <dmitrytsgn@gmail.com>
2019-12-08 23:11:22 +0200

    destroy buy million stars button on IOS

---
+++ Assets/Scripts/DestroyOnIOS.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnIOS : MonoBehaviour {
+
+	// Use this for initialization
+	private void Awake()
+	{
+#if UNITY_IOS
+		Destroy(gameObject);
+#endif
+	}
+}
