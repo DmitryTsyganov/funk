f7c655732fdee9930c52536adc2a2c57f8aa5e1b
DmitryTsyganov <dmitrytsgn@gmail.com>
2020-05-04 21:35:20 +0300

    set up camera to fit all world objects

---
+++ Assets/Scripts/CameraSetUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSetUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var worldScreenHeight = Camera.main.orthographicSize * 2.0;
+		var worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+		float scale = GetComponent<RectTransform>().rect.size.x / (float)worldScreenWidth;
+		Debug.Log("Scale is " + scale);
+		Camera.main.orthographicSize *= scale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/GameSetUp.cs
+++ Assets/Scripts/GameSetUp.cs
@@ -4,8 +4,6 @@ using UnityEngine.UI;
 
 public class GameSetUp : MonoBehaviour {
 
-    public Camera mainCam = new Camera();
-
     public BoxCollider2D topWall/* = new BoxCollider2D()*/;
     public BoxCollider2D bottomWall /*= new BoxCollider2D()*/;
     public BoxCollider2D leftWall /*= new BoxCollider2D()*/;
@@ -15,7 +13,9 @@ public class GameSetUp : MonoBehaviour {
 
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
+        var mainCam = Camera.main;
         topWall.size = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width * 2f, 0f, 0f)).x, wallThickness);
         topWall.offset = new Vector2(0f, mainCam.ScreenToWorldPoint(new Vector3(Screen.width * 2f, 0f, 0f)).y * -1 + wallThickness / 2);
 
@@ -27,18 +27,6 @@ public class GameSetUp : MonoBehaviour {
 
         leftWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2f, 0f)).y);
         leftWall.offset =  new Vector2(mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2f, 0f)).x + wallThickness / 2 * -1, 0f);
-
-        //tests
-
-        //var parser = new XMLParser();
-
-        //parser.makeLevelTest();
-
-        //var level = (Level)parser.parse("test.xml");
-
-        //Debug.Log(level.ball.x);
-        //Debug.Log(level.basket.x);
-        //Debug.Log(level.ObsticleTriangle.x);
     }
 	
 	// Update is called once per frame
